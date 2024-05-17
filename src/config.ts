4a507b89e9d29c0b06e2defdcdd964b9bd906ea8
Ahmed Mohamed <50212311+ig4e@users.noreply.github.com>
2024-05-17 18:59:42 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ const dev = process.env.NODE_ENV === "development";
 const cluster = true ? "cyclic" : "render";
 
 export const API_URL = dev ? "http://localhost:3006" : cluster === "render" ? "https://mtag-api.onrender.com" : "https://good-jade-chimpanzee-tutu.cyclic.app";
-export const IMAGE_PROXY = "https://rule69-proxy.mohamed-saeed-elghamazy.workers.dev";
+export const IMAGE_PROXY = "https://rule69-proxy.moe-dev.workers.dev";
 export const wsrvTemplate = ({ url, q, w, s, n, out }: { url: string; q: number; w: number; s: number; n: 1 | -1; out: "webp" | "jpg" | "png" }) =>
 	`https://wsrv.nl/?url=${encodeURIComponent(url)}&w=${w}&q=${q}&output=${out}${s > 0 ? `&sharp=${s}` : ``}&n=${n}`;
 export const proxyTemplate = ({ url, referer }: { url: string; referer: string }) =>
